ce36348092295172135a712e6bc54abe1e109655
balaprasanna <balaprasannav2009@gmail.com>
2018-08-17 00:57:40 +0800

    Stage 5: Python /api/v1/source API implemented

--- python/app.py
+++ python/app.py
@@ -2,8 +2,11 @@ from flask import Flask, Response, request, Blueprint
 import sys
 from datetime import datetime
 import json
+import requests
+import os
 
 API_VERSION = 'v1'
+SOURCE_URL = "http://api.pnd.gs/v1/sources/"
 # Flask Blueprint
 api = Blueprint('customEndpoints',__name__)
 
@@ -15,14 +18,38 @@ if len(sys.argv) > 1:
 # Create a flask APP object
 app = Flask(__name__)
 
+# **** UTIL FUNCTIONS :Begins****
+
 def sendSuccessResponse(resp_data, status=200, mimetype="application/json"):
     return Response(response=resp_data, status=status, mimetype=mimetype)
+
+def get(url):
+    resp = requests.get(url)
+    if resp.ok:
+        return resp.json()
+
+def get_news_source():
+    content = get(SOURCE_URL)
+    data = {
+        "latest": [],
+        "popular": []
+    }
+    if content:
+        for item in content:
+            data["latest"].append( item["endpoints"]["latest"] )
+            data["popular"].append( item["endpoints"]["popular"] )
+    return data
+
+# **** UTIL FUNCTIONS :Ends****
 
 # Add a base route
 # "/"
 @app.route("/", methods=['GET'])
 def root():
-    resp_data = { "status": "ok" }
+    resp_data = {
+        "status": "ok",
+        "hostname": os.getenv("HOSTNAME")
+        }
     resp_data = json.dumps(resp_data, indent=4)
     return sendSuccessResponse(resp_data)
 
@@ -37,7 +64,7 @@ def time():
 # /api/v1/sources
 @api.route("/source", methods=["GET"])
 def getsources():
-    resp_data = {}
+    resp_data = get_news_source()
     resp_data = json.dumps(resp_data, indent=4)
     return sendSuccessResponse(resp_data)
 
